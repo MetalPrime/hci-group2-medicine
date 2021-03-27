bcaf43aebcc6b5974112b204c545a14bc20dafa9
Jhon Garcia <metalprime57@gmail.com>
2021-03-27 16:36:46 -0500

    update Pantalla

--- scripts/Main.js
+++ scripts/Main.js
@@ -1,39 +1,75 @@
-let pantalla,logica;
+let pantalla, logica;
 
 function setup() {
-    createCanvas(1280, 720);
-    logica = new Logica();
-    initialPantalla = new Pantalla(0,0,'./recursos/clinicaBG.png');
-    
+  createCanvas(1280, 720);
+  logica = new Logica();
+  pantalla = 1;
+  initialPantalla = new Pantalla(0, 0, './recursos/clinicaBG.png');
+  instruccionesPantalla = new Pantalla(0,0);
+
 }
 
 function draw() {
-    pantalla=0;
+  
+  background(0);
 
+  switch (pantalla) {
+    case 0:
+      
 
-    switch(pantalla){
-        case 0:
-         background(0);
-         
-         initialPantalla.pintarPantalla();
-          //play
-          break;
-        case 1:
-          //instruciones
-          break;
-        case 2:
-          //nivel uno
-          logica.pintarNivel1();
-          break;
-        case 3:
-          break;
-      }
+     
+      //play
+      initialPantalla.pintarPantalla();
+      break;
+    case 1:
+      //instruciones
+      instruccionesPantalla.pintarPantalla();
+      break;
+    case 2:
+      //nivel uno
+      logica.pintarNivel1();
+      break;
+    case 3:
+      break;
+  }
 
 }
 
 function mousePressed() {
+  switch (pantalla) {
+    case 0:
 
+      break;
+    case 1:
+
+      break;
+    case 2:
+
+      break;
+    case 3:
+
+      break;
+
+    default:
+      break;
+  }
 }
 function mouseReleased() {
-   
+  switch (pantalla) {
+    case 0:
+
+      break;
+    case 1:
+
+      break;
+    case 2:
+
+      break;
+    case 3:
+
+      break;
+
+    default:
+      break;
+  }
 }
--- scripts/Pantalla.js
+++ scripts/Pantalla.js
@@ -1,18 +1,29 @@
 
-class Pantalla{
+class Pantalla {
 
-    constructor(posX, posY, bg){
+    constructor(posX, posY, bg) {
         this.posX = posX;
         this.posY = posY;
         this.bg = bg;
         this.current = loadImage(bg);
         this.imgBg = loadImage('./recursos/baseBg.jpg');
-        console.log(bg);
     }
 
-      pintarPantalla (){
+    pintarPantalla() {
 
-        image((this.bg==null? this.imgBg: this.current ),this.posY,this.posX,width,height);
-       
+        image((this.bg == null ? this.imgBg : this.current), this.posY, this.posX, width, height);
+
     }
+
+    pintarBtn(posX, posY, sizeX, sizeY,text) {
+        rect(posX,posY,sizeX,sizeY);
+        text(text,posX,posY);
+    }
+
+    sgtePantalla(pantalla) {
+        this.pantalla = pantalla;
+        return this.pantalla++;
+    }
+
+
 }
